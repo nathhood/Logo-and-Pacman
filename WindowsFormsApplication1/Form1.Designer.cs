b405d6516217e5125e6c801665744d7a018a24d6
speed_000 <speed_000@NATHAN-PC>
2016-09-16 13:02:10 -0400

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playButton = new System.Windows.Forms.Button();
+            this.countdowText = new System.Windows.Forms.Label();
+            this.PacManButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.Black;
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.playButton.ForeColor = System.Drawing.Color.White;
+            this.playButton.Location = new System.Drawing.Point(95, 52);
+            this.playButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(196, 79);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play Game";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            this.playButton.MouseLeave += new System.EventHandler(this.PlayButtonHoverOn);
+            this.playButton.MouseHover += new System.EventHandler(this.PlayButtonHoverOff);
+            // 
+            // countdowText
+            // 
+            this.countdowText.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdowText.ForeColor = System.Drawing.Color.White;
+            this.countdowText.Location = new System.Drawing.Point(52, 215);
+            this.countdowText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.countdowText.Name = "countdowText";
+            this.countdowText.Size = new System.Drawing.Size(287, 28);
+            this.countdowText.TabIndex = 1;
+            this.countdowText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.countdowText.Click += new System.EventHandler(this.PlayButtonHoverOff);
+            // 
+            // PacManButton
+            // 
+            this.PacManButton.Location = new System.Drawing.Point(155, 277);
+            this.PacManButton.Name = "PacManButton";
+            this.PacManButton.Size = new System.Drawing.Size(75, 32);
+            this.PacManButton.TabIndex = 2;
+            this.PacManButton.Text = "Run";
+            this.PacManButton.UseVisualStyleBackColor = true;
+            this.PacManButton.Click += new System.EventHandler(this.runPacManButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(379, 321);
+            this.Controls.Add(this.PacManButton);
+            this.Controls.Add(this.countdowText);
+            this.Controls.Add(this.playButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Arcade Central";
+            //this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label countdowText;
+        private System.Windows.Forms.Button PacManButton;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,102 @@
+﻿//This program is created by Nathan Hood
+//This program is created for starting a game
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {         
+            InitializeComponent();
+            PacManButton.Visible = false;
+        }
+
+        private void PlayButtonHoverOff(object sender, EventArgs e) //change the button when hovered on with mouse
+        {
+            playButton.BackColor = Color.Green;
+        }
+
+        private void PlayButtonHoverOn(object sender, EventArgs e) //change back to original button when not hovered on with mouse
+        {
+            playButton.BackColor = Color.Black;
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer three = new SoundPlayer(Properties.Resources.three); //designate the sound file three
+            SoundPlayer two = new SoundPlayer(Properties.Resources.two); //designate the sound file two
+            SoundPlayer one = new SoundPlayer(Properties.Resources.one); //designate the sound file one
+                                                                        
+            Graphics formGraphics = this.CreateGraphics(); //Allow fo graphics to be created
+            SolidBrush whiteBrush = new SolidBrush(Color.White); //Create a white brush
+            SolidBrush redBrush = new SolidBrush(Color.Red); //create a red brush
+            Font BigFont = new Font("Times New Roman", 90, FontStyle.Bold); //create a font to use in the code
+            Font MediumFont = new Font("Courier", 20, FontStyle.Bold); //create a second font to use int eh code
+            Font SmallFont = new Font("Courier", 5, FontStyle.Bold); //create a third font to use in the code
+
+            
+            playButton.Visible = false; //hide the button after clicked
+            
+            /*
+            countdowText.Text = "Game Will Start in: 3"; //change the countdown text to "Game Will Start in 3"
+            three.Play(); //play the sound file three
+            Refresh();
+            Thread.Sleep(1000); //pause for 1 second
+
+            countdowText.Text = "Game Will Start in: 2"; //change the countdown text to "Game Will Start in 2"
+            two.Play(); //play the sound file two
+            Refresh();
+            Thread.Sleep(1000); //pause for 1 second
+
+            countdowText.Text = "Game Will Start in: 1"; //change the countdown text to "Game Will Start in 1"
+            one.Play(); //play the sound file one
+            Refresh();
+            Thread.Sleep(1000); //pause for 1 second
+            */
+            BackColor = Color.Green; //change window background colour to green
+            //countdowText.Text = ""; //change the countdown text to "go"
+            countdowText.Visible = false;
+            Refresh();
+
+            /*formGraphics.FillEllipse(redBrush, 65, 70, 160, 110); //make the oval
+            formGraphics.DrawString("C", BigFont, whiteBrush, 55, 55); //write the large 'c'
+
+            formGraphics.DrawString("RAMS", MediumFont, whiteBrush, 115, 110); //write the medium sized font 'RAMS'
+
+            formGraphics.TranslateTransform(230, 160); //to change where the screen starts
+            formGraphics.RotateTransform(270); //to change the orientation of the screen
+            formGraphics.DrawString("Central Rams Arcade", SmallFont, whiteBrush, new Rectangle()); //to write the sideways text
+            formGraphics.ResetTransform(); //reset the screen orientaion and start
+            Thread.Sleep(1000);
+            */
+            BackColor = Color.Black;
+            PacManButton.Visible = true;
+
+        }
+
+        /*private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }*/
+
+        private void runPacManButton_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+        }
+    }
+}
